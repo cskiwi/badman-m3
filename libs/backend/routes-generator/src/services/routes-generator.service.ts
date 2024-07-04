b5667fa14167ecfc1b8d22405d1f4c0f7fa59f1e
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-07-04 13:02:42 +0200

    feat: prepping for next route generator

--- libs/backend/routes-generator/src/services/routes-generator.service.ts
+++ libs/backend/routes-generator/src/services/routes-generator.service.ts
@@ -3,10 +3,28 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class RoutesGeneratorService {
   generateRoutesTxt() {
+    const routes: string[] = ['/', '/players'];
+    const topPlayers = this.getTopPlayers();
+    const mostSearchedPlayers = this.getMostSearchedPlayers();
 
-    return `
-/
-/players
-    `;
+    topPlayers.forEach((player) => {
+      routes.push(`/${player}`);
+    });
+
+    mostSearchedPlayers.forEach((player) => {
+      routes.push(`/${player}`);
+    });
+
+    return routes.join('\n');
+  }
+
+  // get top players
+  private getTopPlayers() {
+    return [];
+  }
+
+  // get most searched players
+  private getMostSearchedPlayers() {
+    return ['glenn-latomme'];
   }
 }
