825bee61ba416a11ef5ea8f73091770cd71dbd78
cskiwi <glenn.latomme@gmail.com>
2024-07-24 17:18:11 +0200

    ci: build for production?

--- scripts/build.js
+++ scripts/build.js
@@ -112,7 +112,7 @@ async function main() {
     console.log('Routes saved to routes.txt');
 
     console.log('Building app...');
-    await runCommandAndWait(`nx`, ['build', 'app']);
+    await runCommandAndWait(`nx`, ['build', 'app', '--configuration production']);
     console.log('App build completed');
 
     console.log('Stopping server...');
