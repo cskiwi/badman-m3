1dd9350c9310c67683aad3d72559fd5c5051d79c
cskiwi <glenn.latomme@gmail.com>
2024-07-24 16:30:38 +0200

    fix: my bad

--- libs/frontend/pages/club/src/pages/overview/page-overview.service.ts
+++ libs/frontend/pages/club/src/pages/overview/page-overview.service.ts
@@ -85,7 +85,7 @@ export class OverviewService {
             clubs(where: $where) {
               id
               clubId
-              fullName
+              name
               slug
             }
           }
