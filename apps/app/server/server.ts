fd1a5846e335bd54a6bcc29a1e1843ede98f1ed2
cskiwi <glenn.latomme@gmail.com>
2024-09-11 16:35:48 +0200

    maybe this?

--- apps/app/server/server.ts
+++ apps/app/server/server.ts
@@ -7,6 +7,9 @@ import { REQUEST as COOKIE_SERVICE_REQ } from 'ngx-cookie-service-ssr';
 import { dirname, join, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import bootstrap from '../src/main.server';
+import { config } from 'dotenv';
+
+config();
 
 // The Express app is exported so that it can be used by serverless Functions.
 export async function app() {
