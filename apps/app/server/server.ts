dfd4515e24faa7be551cfb602909dee02add2952
cskiwi <glenn.latomme@gmail.com>
2024-09-11 16:45:51 +0200

    reverting the server to the working version

--- apps/app/server/server.ts
+++ apps/app/server/server.ts
@@ -7,70 +7,59 @@ import { REQUEST as COOKIE_SERVICE_REQ } from 'ngx-cookie-service-ssr';
 import { dirname, join, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import bootstrap from '../src/main.server';
-import { config } from 'dotenv';
-
-config();
 
 // The Express app is exported so that it can be used by serverless Functions.
 export async function app() {
   const server = express();
-  // const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  // const browserDistFolder = resolve(serverDistFolder, '../browser');
-  // const indexHtml = join(serverDistFolder, 'index.server.html');
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
+  const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  // const commonEngine = new CommonEngine();
+  const commonEngine = new CommonEngine();
 
-  // server.set('view engine', 'html');
-  // server.set('views', browserDistFolder);
+  server.set('view engine', 'html');
+  server.set('views', browserDistFolder);
 
-  // // Serve static files from /browser
-  // server.get(
-  //   '**',
-  //   express.static(browserDistFolder, {
-  //     maxAge: '1y',
-  //     index: 'index.html',
-  //   }),
-  // );
+  // Serve static files from /browser
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    }),
+  );
 
-  // // All regular routes use the Angular engine for rendering
-  // // except for /api/** routes
-  // server.get('**', (req, res, next) => {
-  //   const { protocol, originalUrl, baseUrl, headers } = req;
+  // All regular routes use the Angular engine for rendering
+  // except for /api/** routes
+  server.get('**', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
 
-  //   if (shouldSkip(req.originalUrl)) {
-  //     // Handle API routes separately
-  //     next();
-  //   } else {
-  //     commonEngine
-  //       .render({
-  //         bootstrap,
-  //         documentFilePath: indexHtml,
-  //         url: `${protocol}://${headers.host}${originalUrl}`,
-  //         publicPath: browserDistFolder,
-  //         providers: [
-  //           { provide: APP_BASE_HREF, useValue: baseUrl },
-  //           { provide: COOKIE_SERVICE_REQ, useValue: req },
-  //           { provide: 'RESPONSE', useValue: res },
-  //         ],
-  //       })
-  //       .then((html) => res.send(html))
-  //       .catch((err) => next(err));
-  //   }
-  // });
+    if (shouldSkip(req.originalUrl)) {
+      // Handle API routes separately
+      next();
+    } else {
+      commonEngine
+        .render({
+          bootstrap,
+          documentFilePath: indexHtml,
+          url: `${protocol}://${headers.host}${originalUrl}`,
+          publicPath: browserDistFolder,
+          providers: [
+            { provide: APP_BASE_HREF, useValue: baseUrl },
+            { provide: COOKIE_SERVICE_REQ, useValue: req },
+            { provide: 'RESPONSE', useValue: res },
+          ],
+        })
+        .then((html) => res.send(html))
+        .catch((err) => next(err));
+    }
+  });
 
   // setup NestJS app
-  // const adapter = new ExpressAdapter(server);
-  // const app = await getServer(adapter);
+  const adapter = new ExpressAdapter(server);
+  const app = await getServer(adapter);
 
-  // await app.init();
-  // adapter.init();
-
-  console.log('Server started', process.env);
-
-  // serve some hello world
-  server.get('/', (req, res) => {
-    res.json(process.env);
-  });
+  await app.init();
 
   return server;
 }
